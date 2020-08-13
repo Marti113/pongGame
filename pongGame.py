5f55140fb0c8080dd492bf51379e57b4a1feaeff
Marti Kandallu <marti.kandallu@gmail.com>
2020-08-12 19:17:06 -0700

    paddle movement

--- pongGame.py
+++ pongGame.py
@@ -41,10 +41,16 @@ def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
     paddle_a.sety(y)
+    
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
 
 # Keyboard binding
-wn.listen()
-wn.onkeypress(paddle_a_up, "w")    
+wn.listen() 
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")    
 
 # Main game loop
 
